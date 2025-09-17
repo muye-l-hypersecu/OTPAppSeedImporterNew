d09e34f775a5f5ba7b17a6401ab1d83917886469
Mu Ye Liu <muye.l@hypersecu.com>
2025-09-17 11:57:12 -0700

    fixed some small errors

--- OTPAppSeedImporterNew/OTPAppSeedImporter.cs
+++ OTPAppSeedImporterNew/OTPAppSeedImporter.cs
@@ -12,7 +12,7 @@ namespace OTPAppSeedImporterNew
         private void OTPAppSeedImporter_Load(object sender, EventArgs e)
         {
             SeedFileParser.DoThis(); // This is just a dummy function, remove later. 
-            DatabaseManager.DoThisManager(); // Another dummy function, remove later.
+            DatabaseManager.DoThisManager(); // Another dummy function, remove later. 
         }
     }
 }
