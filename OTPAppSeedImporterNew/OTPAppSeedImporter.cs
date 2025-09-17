c3c4e99d8676d8d2c4b58ae5480793d66afcd3de
Mu Ye Liu <muye.l@hypersecu.com>
2025-09-17 10:25:02 -0700

    created a windows form

---
+++ OTPAppSeedImporterNew/OTPAppSeedImporter.cs
@@ -0,0 +1,10 @@
+namespace OTPAppSeedImporterNew
+{
+    public partial class OTPAppSeedImporter : Form
+    {
+        public OTPAppSeedImporter()
+        {
+            InitializeComponent();
+        }
+    }
+}
