921e1ca3e649dfb05f36b49aa4f8f712c482f5e3
Mu Ye Liu <muye.l@hypersecu.com>
2025-11-10 13:42:17 -0800

    updated the init sql table

--- OTPAppSeedImporterNew/Utility/Init.sql
+++ OTPAppSeedImporterNew/Utility/Init.sql
@@ -1,20 +1,42 @@
--- Creates the table for the token spec
-CREATE TABLE IF NOT EXISTS ft_tokeninfo (
-	serialNumber VARCHAR(20) PRIMARY KEY,
-	seed VARCHAR(128) NOT NULL,
-	specId VARCHAR(6) NOT NULL,
-	importTime DATETIME DEFAULT (datetime('now', 'localtime')),
-	FOREIGN KEY (specID) REFERENCES ft_tokenspec(specID) ON DELETE CASCADE ON UPDATE CASCADE
- );
+-- IF DATABASE IS NOT INITIALZIED YET, RUN THE SQL BELOW!!
 
- -- Creates the table for the token info
- CREATE TABLE IF NOT EXISTS ft_tokenspec (
-	specId VARCHAR(6) PRIMARY KEY,
-	token_interval INTEGER NOT NULL
- );
+-- Creates the table for the token info
+CREATE TABLE ft_tokeninfo (
+    tknid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    token varchar(32) NOT NULL,
+    pubkey varchar(1024) NOT NULL DEFAULT '',
+    authnum varchar(32) DEFAULT '0',
+    physicaltype INTEGER DEFAULT 0,
+    producttype INTEGER DEFAULT 0,
+    specid varchar(32) NOT NULL,
+    importtime INTEGER DEFAULT 0,
+    pubkeystate INTEGER DEFAULT 0,
+    pubkeyfactor varchar(128),
+    tknifmid INTEGER NOT NULL,
+    tknofmid INTEGER NOT NULL,
+    crust varchar(32),
+    tknexttype varchar(128),
+    tkntype INTEGER NOT NULL,
+    tknstate INTEGER,
+    tksendnumber varchar(64),
+    tknvaliddate INTEGER,
+    CONSTRAINT fk_tkninfo_tknspec_specid FOREIGN KEY (specid) REFERENCES ft_tokenspec (specid) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (token ASC)
+);
+
+
+-- Creates the table for the token spec
+CREATE TABLE ft_tokenspec (
+    specid INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT NOT NULL,                       
+    sn_length INTEGER NOT NULL,              
+    token_interval INTEGER NOT NULL,         
+    checksum TEXT NOT NULL,      
+    algorithm TEXT NOT NULL                
+);
 
  -- Insert the 3 required token spec types
- INSERT OR IGNORE INTO ft_tokenspec(specId, token_interval) VALUES 
-	("TOTP30", 30),
-	("TOTP60", 60),
-	("HOTP", 0);
+ INSERT INTO ft_tokenspec (specid, sn_length, token_interval, checksum, algorithm) VALUES 
+	('TOTP30', 12, 30, 'luhn', 'SHA1'),
+    ('TOTP60', 12, 60, 'luhn', 'SHA1'),
+    ('HOTP', 12, 0, 'luhn', 'SHA1'); 
