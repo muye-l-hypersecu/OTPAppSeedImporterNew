537a7194b1344a50aadedd1fb402083e1f22e1fe
Mu Ye Liu <muye.l@hypersecu.com>
2025-11-10 15:35:59 -0800

    restored the test project

---
+++ Tests/ModelTest/SeedEntryTest.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+
+namespace ModelTest;
+
+// Represents the test class for the seed entry class
+public class SeedEntryTest
+{
+    public SeedEntry seedEntry;
+
+    // Sets up a seed entry
+    [SetUp]
+    public void SetUp()
+    {
+        seedEntry = new SeedEntry("2345823858", "AFECD15568716589DF7279865");
+    }
+
+    // Tests to make sure the instances are set up properly
+    [Test]
+    public void SetUpTest()
+    {
+        Assert.That(seedEntry.GetSerialNumber(), Is.EqualTo("2345823858"));
+        Assert.That(seedEntry.GetSeed(), Is.EqualTo("AFECD15568716589DF7279865"));
+    }
+}
+
